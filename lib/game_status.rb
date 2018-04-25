83e11a89af2f3a5b83aa45f9094cb34c3bb9ce11
Alex Wilson <fonzmp@gmail.com>
2018-04-25 18:22:31 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -29,8 +29,9 @@ def won?(board)
       position_2 = board[win_index_2]
       position_3 = board[win_index_3]
 
-      if [position_1, position_2, position_3].all?{|letter| letter == "X" || letter = "O"}
+      if position_1 == "X" && position_2 == "X" && position_3 == "X"
         return win_combination
+      elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
       end
     end
     return false
