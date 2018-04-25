69c43cb123f857fe0b1bb54f34a996873872d112
Alex Wilson <fonzmp@gmail.com>
2018-04-25 17:21:29 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -22,7 +22,7 @@ def won?(board)
     for win_combination in WIN_COMBINATIONS do
       win_index_1 = win_combination[0]
       win_index_2 = win_combination[1]
-      win_index_2 = win_combination[2]
+      win_index_3 = win_combination[2]
 
       position_1 = board[win_index_1]
       position_2 = board[win_index_2]
