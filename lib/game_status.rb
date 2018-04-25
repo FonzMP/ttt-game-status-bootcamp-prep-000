d5cc4db96ef10f5d39ebacb813b577935f644ad9
Alex Wilson <fonzmp@gmail.com>
2018-04-25 17:23:17 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,7 +19,7 @@ def won?(board)
   if board.all? == "" || board.all? == " "
     return false
   else
-    for win_combination in WIN_COMBINATIONS do
+    for WIN_COMBINATIONS.each do |win_combination|
       win_index_1 = win_combination[0]
       win_index_2 = win_combination[1]
       win_index_3 = win_combination[2]
