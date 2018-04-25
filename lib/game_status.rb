3a3a87afb7e5fa8c61223c52a56ec8f71e745c8f
Alex Wilson <fonzmp@gmail.com>
2018-04-25 18:37:32 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -32,7 +32,7 @@ def won?(board)
       if position_1 == "X" && position_2 == "X" && position_3 == "X"
         return win_combination_x = win_combination
       elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-        return win_combination
+        return win_combination_o = win_combination
       end
     end
     return false
